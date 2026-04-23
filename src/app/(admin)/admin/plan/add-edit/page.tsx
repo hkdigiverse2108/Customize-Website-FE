40d6c2e6c8891551d5ab6d6b14f403ee809ec15c
hk-krish <krishhkdigiverse@gmail.com>
2026-04-23 14:23:32 +0530

    feat: create add-edit plan page with formik validation fields

--- src/app/(admin)/admin/plan/add-edit/page.tsx
+++ src/app/(admin)/admin/plan/add-edit/page.tsx
@@ -3,7 +3,7 @@
 import { CommonButton, CommonValidationSelect, CommonValidationSwitch, CommonValidationTextField } from "@/attribute";
 import { CommonCard } from "@/components/common";
 import { PAGE_TITLE } from "@/constants";
-import { SUBSCRIPTION_TYPE_OPTIONS } from "@/data";
+import { PLAN_DURATION_OPTIONS, SUBSCRIPTION_TYPE_OPTIONS } from "@/data";
 import { Row } from "antd";
 import { Form, Formik } from "formik";
 
@@ -19,16 +19,16 @@ const AddEditPlanPage = () => {
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form className="space-y-6">
           <Row gutter={[16, 16]} className="mb-2">
-            <CommonValidationSelect name="name" label="name" options={SUBSCRIPTION_TYPE_OPTIONS} mode="multiple" col={{ xs: 24, sm: 12 }} />
             <CommonValidationSelect name="name" label="name" options={SUBSCRIPTION_TYPE_OPTIONS} col={{ xs: 24, sm: 12 }} />
             <CommonValidationTextField name="price" type="number" label="price" required col={{ xs: 24, sm: 12 }} />
-            <CommonValidationTextField name="duration" label="duration" required col={{ xs: 24, sm: 12 }} />
-            <CommonValidationTextField name="themeLimit" type="number" label="themeLimit" required col={{ xs: 24, sm: 12 }} />
-            <CommonValidationTextField name="productLimit" type="number" label="productLimit" required col={{ xs: 24, sm: 12 }} />
-            <CommonValidationTextField name="blogLimit" type="number" label="blogLimit" required col={{ xs: 24, sm: 12 }} />
-            <CommonValidationTextField name="orderLimit" type="number" label="orderLimit" required col={{ xs: 24, sm: 12 }} />
+            <CommonValidationSelect name="duration" label="duration" options={PLAN_DURATION_OPTIONS} col={{ xs: 24, sm: 12 }} />
+            <CommonValidationTextField name="themeLimit" type="number" label="theme Limit" required col={{ xs: 24, sm: 12 }} />
+            <CommonValidationTextField name="productLimit" type="number" label="product Limit" required col={{ xs: 24, sm: 12 }} />
+            <CommonValidationTextField name="blogLimit" type="number" label="blog Limit" required col={{ xs: 24, sm: 12 }} />
+            <CommonValidationTextField name="orderLimit" type="number" label="order Limit" required col={{ xs: 24, sm: 12 }} />
             <CommonValidationSelect name="features" label="features" options={[]} mode="tags" col={{ xs: 24, sm: 12 }} />
-            <CommonValidationSwitch name="isPopular" label="isPopular" col={{ xs: 24 }} />
+            <CommonValidationSwitch name="customDomainSupport" label="custom Domain Support" col={{ xs: 24 }} />
+            <CommonValidationSwitch name="isActive" label="isActive" col={{ xs: 24 }} />
           </Row>
           <CommonButton title="Submit" htmlType="submit" loading={false} />
         </Form>
