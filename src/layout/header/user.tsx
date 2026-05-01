997dd27882da0d3a5069b1cd08ba802547f0e12f
HK-ankithkdigiverse <ankithkdigiverse@gmail.com>
2026-05-01 09:47:22 +0530

    userProfile

--- src/layout/header/user.tsx
+++ src/layout/header/user.tsx
@@ -1,6 +1,7 @@
 import { Queries } from "@/api";
 import { CommonProfileAvatar } from "@/components/common";
 import { ROUTES } from "@/constants";
+import { ACCOUNT_TYPE } from "@/data";
 import { setSignOut, useAppDispatch, useAppSelector } from "@/store";
 import { useClickOutside } from "@/utils";
 import Link from "next/link";
@@ -8,20 +9,20 @@ import { memo, useCallback } from "react";
 import { shallowEqual } from "react-redux";
 
 const User = () => {
-  const { data: storeData } = Queries.useGetStore({});
-  const activeStore = storeData?.data?.stores?.[0];
-  const storeName = activeStore?.name;
-  const storeLogo = activeStore?.logo?.[0];
-
-
   const user = useAppSelector(
     (state) => ({
       firstName: state.auth.user?.firstName,
       lastName: state.auth.user?.lastName,
       email: state.auth.user?.email,
+      role: state.auth.user?.role,
     }),
     shallowEqual,
   );
+
+  const { data: storeData } = Queries.useGetStore({});
+  const activeStore = storeData?.data?.stores?.[0];
+  const storeName = user.role === ACCOUNT_TYPE.ADMIN ? "" : activeStore?.name;
+  const storeLogo = user.role === ACCOUNT_TYPE.ADMIN ? "" : activeStore?.logo?.[0];
 
   const { open, setOpen, wrapperRef } = useClickOutside();
   const dispatch = useAppDispatch();
@@ -72,7 +73,7 @@ const User = () => {
 
         <ul className="flex flex-col p-1.5">
           <li>
-            <Link href={ROUTES.STORE.DASHBOARD}>
+            <Link href={user.role === ACCOUNT_TYPE.ADMIN ? ROUTES.ADMIN.DASHBOARD : ROUTES.STORE.DASHBOARD}>
               <div className="flex items-center gap-3 px-3 py-2.5 font-medium text-gray-600 rounded-xl group text-theme-sm hover:bg-brand-50 hover:text-brand-600 dark:text-gray-400 dark:hover:bg-brand-500/10 dark:hover:text-brand-400 transition-all duration-200">
                 <svg className="fill-gray-500 text-gray-500 group-hover:fill-brand-600 group-hover:text-brand-600 dark:fill-gray-400 dark:text-gray-400 dark:group-hover:fill-brand-400 dark:group-hover:text-brand-400 transition-colors duration-200" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" clipRule="evenodd" d="M12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 14.1526 4.3002 16.1184 5.61936 17.616C6.17279 15.3096 8.24852 13.5955 10.7246 13.5955H13.2746C15.7509 13.5955 17.8268 15.31 18.38 17.6167C19.6996 16.119 20.5 14.153 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5ZM17.0246 18.8566V18.8455C17.0246 16.7744 15.3457 15.0955 13.2746 15.0955H10.7246C8.65354 15.0955 6.97461 16.7744 6.97461 18.8455V18.856C8.38223 19.8895 10.1198 20.5 12 20.5C13.8798 20.5 15.6171 19.8898 17.0246 18.8566ZM2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11.9991 7.25C10.8847 7.25 9.98126 8.15342 9.98126 9.26784C9.98126 10.3823 10.8847 11.2857 11.9991 11.2857C13.1135 11.2857 14.0169 10.3823 14.0169 9.26784C14.0169 8.15342 13.1135 7.25 11.9991 7.25ZM8.48126 9.26784C8.48126 7.32499 10.0563 5.75 11.9991 5.75C13.9419 5.75 15.5169 7.32499 15.5169 9.26784C15.5169 11.2107 13.9419 12.7857 11.9991 12.7857C10.0563 12.7857 8.48126 11.2107 8.48126 9.26784Z" fill="currentColor" />
